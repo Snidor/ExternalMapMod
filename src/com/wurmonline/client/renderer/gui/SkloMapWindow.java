fa7998a569736bdd73ebf3fa8e917df3c952c951
Snidor <Snidor@users.noreply.github.com>
2020-05-27 18:07:20 +0200

    auto server detection implemented

--- src/com/wurmonline/client/renderer/gui/SkloMapWindow.java
+++ src/com/wurmonline/client/renderer/gui/SkloMapWindow.java
@@ -18,9 +18,13 @@ public class SkloMapWindow implements TerrainChangeListener
 {
     final World mWorld;
     private BufferedImage mMap;
+    private BufferedImage mOriginal;
 	private JFrame mFrame = new JFrame("Transparent Window");
 	private JLabel mLabel;
 	private ImageIcon mIcon;
+	private Image mThumbnail;
+	private String mPath;
+	private String mServerName;
 	private static SkloMapWindow mInstance;
 	public static Logger mLogger = Logger.getLogger( "SkloMapWindow" );
 	
@@ -28,6 +32,7 @@ public class SkloMapWindow implements TerrainChangeListener
 	{
 		this.mWorld = pWorld;
 		this.mWorld.getNearTerrainBuffer().addListener( this );
+		this.mServerName = pWorld.getServerName();
 	}
 	
 	public void start() 
@@ -41,18 +46,16 @@ public class SkloMapWindow implements TerrainChangeListener
         this.mFrame = new JFrame( "Sklo Map Window" );        
         try 
         {
-            String lPath = "https://sklotopolis.ddns.net/unlimited/2/mapdump-flat.png";
-            URL lURL = new URL( lPath );
-            BufferedImage lTmp = ImageIO.read( lURL );
-            Image thumbnail = lTmp.getScaledInstance( 512, 512, Image.SCALE_SMOOTH);
-            mMap = new BufferedImage(thumbnail.getWidth(null), thumbnail.getHeight(null), BufferedImage.TYPE_INT_RGB);
-            mMap.getGraphics().drawImage(thumbnail, 0, 0, null);
+            loadMap( mWorld.getServerName() );
+            mThumbnail = mOriginal.getScaledInstance( 512, 512, Image.SCALE_SMOOTH );
+            mMap = new BufferedImage( mThumbnail.getWidth( null ), mThumbnail.getHeight( null ), BufferedImage.TYPE_INT_RGB );
+            mMap.getGraphics().drawImage( mThumbnail, 0, 0, null );
             mIcon = new ImageIcon( mMap );
             mLabel = new JLabel( mIcon );
-            mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
             mFrame.getContentPane().add( mLabel );
             mFrame.pack();
-            mFrame.setLocation(200, 200);
+            mFrame.setLocation( 200, 200 );
             mFrame.setAlwaysOnTop( true );
             mFrame.setVisible(true);
             mFrame.setResizable(false);
@@ -63,16 +66,51 @@ public class SkloMapWindow implements TerrainChangeListener
         catch (Exception exp) 
         {   	
             exp.printStackTrace();
-        }
+        } catch (Throwable e) 
+        {
+			e.printStackTrace();
+		}
         updatePosition();
     }
 	
-	public void updatePosition()
+	private void loadMap( String pServername ) throws Throwable
 	{
+		switch( pServername )
+		{
+			case "Liberty":
+				mPath = "https://sklotopolis.ddns.net/unlimited/2/mapdump-flat.png";
+				break;
+			case "Novus":
+				mPath = "https://sklotopolis.ddns.net/unlimited/3/mapdump-flat.png";
+				break;
+			case "Caza":
+				mPath = "https://sklotopolis.ddns.net/unlimited/4/mapdump-flat.png";
+				break;
+			default:
+				mPath = "https://sklotopolis.ddns.net/unlimited/1/mapdump-flat.png";
+		}
+        URL lURL = new URL( mPath );
+        mOriginal = ImageIO.read( lURL );
+	}
+	
+	private void updatePosition()
+	{
+		if ( !mServerName.contentEquals( mWorld.getServerName() ) )
+		{
+			try 
+			{
+				mServerName = mWorld.getServerName();
+				loadMap( mWorld.getServerName() );
+			} catch (Throwable e) 
+			{
+				e.printStackTrace();
+			}
+		}
 		int lPX = mWorld.getPlayerCurrentTileX() / 8;
 		int lPY = mWorld.getPlayerCurrentTileY() / 8;
 		int[] lColor = { 255, 20 , 147 };
-		BufferedImage lMap = mMap;
+		BufferedImage lMap = new BufferedImage( mThumbnail.getWidth( null ), mThumbnail.getHeight( null ), BufferedImage.TYPE_INT_RGB );
+        lMap.getGraphics().drawImage( mThumbnail, 0, 0, null );
 		for ( int i = lPX - 2; i <= lPX + 2; i ++ )
 		{
 			for ( int j = lPY - 2; j <= lPY + 2; j ++ )
@@ -84,13 +122,11 @@ public class SkloMapWindow implements TerrainChangeListener
 		mIcon = new ImageIcon( lMap );
 		mLabel.setIcon( mIcon );
 		mLabel.repaint();
-		mLogger.log( Level.INFO, "UPDATE POSITION" );
 	}
 
 	@Override
 	public void terrainUpdated(int arg0, int arg1, int arg2, int arg3, boolean arg4, boolean arg5) 
 	{
-		mLogger.log( Level.INFO, "updated terrain" );
 		updatePosition();
 	}
 }
